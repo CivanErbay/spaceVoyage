5d8f085111130413a5b6b0c97a26cf46e6cbc5fd
Civan Erbay <Civan.Erbay@web.de>
2020-05-17 13:02:20 +0200

    Add P1.1

--- AppMain.java
+++ AppMain.java
@@ -17,9 +17,22 @@ public class AppMain {
         //Assignment of the Ship
         SpaceShip myShip = GameMethods.pickShip(shipIndex-1);
         System.out.println(myShip);
+
+        //Journey-P1-D1
+        System.out.println("Year 2448:");
+        System.out.println("You are the captain of a famous scouting unit, which is responsible for the mapping of new space sectors in the infinite depths of the universe ");
         System.out.println("You and your crew enter " + myShip.name);
         System.out.println("After starting the engine your navigator asks for the direction: ");
-        System.out.println("My captain: In which space system you want to jump? Arcturus Nebula or Beta Astoria?");
+        System.out.println("My captain: In which space system you want to jump? Arcturus Nebula('1') or Beta Astoria('2')?");
+        Scanner p1Empty = new Scanner(System.in);
+        int p1 = p1Empty.nextInt();
+        System.out.println(p1 == 1 ? "Your ship the " + myShip.name + "jumps to Arcturus Nebula.": "Your ship the " + myShip.name + "jumps to Beta Astoria."
+        );
+
+        //Journey-P1.1 - Arcturus
+        System.out.println("You and your crew arrived savely in Arcturus Nebula");
+        System.out.println("");
+        //Journey-P1.2 - Beta
 
         //Fight
        /* boolean fightResult = GameMethods.isWinFight(myShip,GameMethods.enemyShip);
