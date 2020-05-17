f9e37705ffb4d38a950d7ae2911fed3a71132494
Civan Erbay <Civan.Erbay@web.de>
2020-05-17 13:27:10 +0200

    Change inital storyline

--- AppMain.java
+++ AppMain.java
@@ -21,7 +21,10 @@ public class AppMain {
         //Journey-P1-D1
         System.out.println("Year 2448:");
         System.out.println("You are the captain of a famous scouting unit, which is responsible for the mapping of new space sectors in the infinite depths of the universe ");
-        System.out.println("You and your crew enter " + myShip.name);
+        System.out.println("Right now you are on the way back from a successful mission in the outer rim. You are awaiting a huge reward back at the headquarters of the federal alliance on planet earth. ");
+        System.out.println("While on the journey back, one of your officers is asking for your attention. He reports an strange observation in the Arcturues Nebula Section - usually a very calm but very well known place not far from your actual position");
+        System.out.println("'Sir, we receive very uncommon signals from this section.. it seems like an entire area has disappeared there.'");
+        System.out.println("As the Commander of Royal-Scouts, the most demanded and renowned scouting unit of the federal alliance, it is your actual duty to check these kind of Uncertainties.  ");
         System.out.println("After starting the engine your navigator asks for the direction: ");
         System.out.println("My captain: In which space system you want to jump? Arcturus Nebula('1') or Beta Astoria('2')?");
         Scanner p1Empty = new Scanner(System.in);
